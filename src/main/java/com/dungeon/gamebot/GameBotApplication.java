23f02a637197b4452d8f7b52acd140c36bc4cb66
yfhpfy123 <ivash_max@mail.ru>
2025-05-09 00:47:48 +0300

    Start

---
+++ src/main/java/com/dungeon/gamebot/GameBotApplication.java
@@ -0,0 +1,13 @@
+package com.dungeon.gamebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameBotApplication.class, args);
+    }
+
+}
